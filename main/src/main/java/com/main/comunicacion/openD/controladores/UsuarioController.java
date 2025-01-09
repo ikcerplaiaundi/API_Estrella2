39a20458b4734458ecbc0d75e8491100bf8c7d8f
Mijael <mijael30@gmail.com>
2025-01-09 08:08:24 +0100

    merge

--- main/src/main/java/com/main/comunicacion/openD/controladores/UsuarioController.java
+++ main/src/main/java/com/main/comunicacion/openD/controladores/UsuarioController.java
@@ -6,22 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.main.comunicacion.openD.DTOs.UsuarioDTO;
 import com.main.modelo.entidades.Usuario;
 import com.main.modelo.repositorios.UsuarioRepositorio;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
 
-    @GetMapping
-    public ResponseEntity<List<Usuario>> obtenerUsuarios() {
+    @GetMapping("/api/usuarios")
+    public ResponseEntity<List<UsuarioDTO>> obtenerUsuarios() {
         List<Usuario> usuarios = usuarioRepositorio.findAll();
-        return ResponseEntity.ok(usuarios);
+        List<UsuarioDTO> usuariosDTO = usuarios.stream()
+            .map(usuario -> new UsuarioDTO(
+                usuario.getId(),
+                usuario.getNombre(),
+                usuario.getCorreo(),
+                usuario.getContraseña(),
+                usuario.getRol().getName() 
+            ))
+            .collect(Collectors.toList());
+        return ResponseEntity.ok(usuariosDTO);
     }
+
 
 }
