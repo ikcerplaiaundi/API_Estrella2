be577f70ee9f047110539a1df26f58dd8cd85c3d
Irene2315 <ireneherrero@outlook.com>
2025-01-09 12:11:49 +0100

    subida_ruta_optener_ciudades

---
+++ main/src/main/java/com/main/comunicacion/privadas/servicios/CiudadPrivateService.java
@@ -0,0 +1,29 @@
+package com.main.comunicacion.privadas.servicios;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.main.comunicacion.mapeos.CiudadPrivateMapper;
+import com.main.comunicacion.mapeos.IncidenciaPrivateMapper;
+import com.main.comunicacion.privadas.DTOs.CiudadPrivateDTO;
+import com.main.modelo.entidades.Ciudad;
+import com.main.modelo.repositorios.CiudadRepositorio;
+
+@Service
+public class CiudadPrivateService {
+
+    private final CiudadRepositorio ciudadRepositorio;
+
+    public CiudadPrivateService(CiudadRepositorio ciudadRepositorio) {
+        this.ciudadRepositorio = ciudadRepositorio;
+    }
+
+    public List<CiudadPrivateDTO> obtenerCiudades() {
+        List<Ciudad> ciudades = ciudadRepositorio.findAll();
+        return ciudades.stream()
+                .map(CiudadPrivateMapper::toCiudadDTO)
+                .collect(Collectors.toList());
+    }
+}
