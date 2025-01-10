4c1916d410bf1546bf98b3bd06f0281f231a35f0
Irene2315 <ireneherrero@outlook.com>
2025-01-10 09:33:34 +0100

    todas_las_incidencias

--- main/src/main/java/com/main/modelo/entidades/Region.java
+++ main/src/main/java/com/main/modelo/entidades/Region.java
@@ -1,6 +1,16 @@
 package com.main.modelo.entidades;
 
-import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "regiones")
