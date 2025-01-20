1608d963d66df1e97ec62c03b7c5e270ed5360ce
Mijael <mijael30@gmail.com>
2025-01-20 08:21:33 +0100

    Modificación roles

--- main/src/main/java/com/main/comunicacion/privadas/controladores/RolController.java
+++ main/src/main/java/com/main/comunicacion/privadas/controladores/RolController.java
@@ -11,7 +11,7 @@ import com.main.modelo.repositorios.RolRepositorio;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api/roles")
+@RequestMapping("/roles")
 public class RolController {
 
     @Autowired
